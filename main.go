acbb24ab8c3336a2c125ee2588abe43b786b74a6
1nv8rZim <msf9542@rit.edu>
2024-09-14 22:15:45 -0400

    update schema

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 )
 
 type Schema struct {
-	Target string `json:"target"`
-	Port   int    `json:"port"`
+	Target   string `json:"target"`
+	Port     int    `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	LDAPS    bool   `json:"ldaps"`
 }
 
 func Run(ctx context.Context, config string) error {
